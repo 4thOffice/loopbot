ed8cb7a2d4b7fb9e21c696e49f77ef5afdc041ff
Niko012 <snebergerniko@gmail.com>
2023-09-25 11:39:08 +0200

    Add jsonified prompt api data return

--- loopBotServer.py
+++ loopBotServer.py
@@ -26,7 +26,7 @@ def get_answer():
 def get_prompt():
     prompt = lb.getPrompt()
     print("prompt:\n", prompt)
-    return prompt
+    return jsonify(prompt)
 
 @app.route('/vote', methods=['PUT'])
 def handle_vote():
