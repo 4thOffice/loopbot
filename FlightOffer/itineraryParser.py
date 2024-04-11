6084c62222c7f6b56283747777a54863545785d9
Niko012 <snebergerniko@gmail.com>
2024-04-11 09:52:17 +0200

    Improve itinerary parser robustness

--- FlightOffer/itineraryParser.py
+++ FlightOffer/itineraryParser.py
@@ -51,13 +51,14 @@ def parse_date(date_str):
 def parse_flight_info(text):
     # Define regular expression patterns to extract information
     pattern = r"""
-        (?P<airline_code>[A-Z]{2})  # Airline IATA code - 2 letters
+        (?P<airline_code>[A-Za-z]{2})  # Airline IATA code - 2 letters
         \s*(?P<flight_number>\d+)  # Flight number - digits
-        \s+[A-Z]\s+  # Skip the placeholder character and spaces
+        (?:\s+)?  # Skip optional spaces
+        (?:[A-Za-z]\s+)?  # Skip the placeholder character 'X' and spaces if present
         (?P<date>\d{1,2}[A-Z]{3})  # Date - day and 3-letter month
-        \s+\d\s+  # Skip day of the week and spaces
-        (?P<source>[A-Z]{3})  # Source airport code - 3 letters
-        (?P<destination>[A-Z]{3})  # Destination airport code - 3 letters
+        (?:\s+\d\s+)?(.+)  # Skip day of the week and spaces
+        (?P<source>[A-Za-z]{3})  # Source airport code - 3 letters
+        (?P<destination>[A-Za-z]{3})  # Destination airport code - 3 letters
         .*?  # Skip placeholder and spaces
         (?P<departure_time>\d{4}(?:\+\d+)?)   # Departure time - 4 digits
         \s+
@@ -85,17 +86,17 @@ def parse_offer_text(text):
                 days_offset = int(offset_split[1])
 
             duration = calculate_time_difference(flight["departure_time"], flight["arrival_time"])
-            flights.append({"departure": {"iataCode": flight["source"], "at": format_departure_time(flight['date'], flight["departure_time"], 0)}, "arrival": {"iataCode": flight["destination"], "at": format_departure_time(flight['date'], flight["arrival_time"], days_offset)}, "flightNumber": flight["flight_number"], "carrierCode": flight["airline_code"], "duration": duration, "iteraryNumber": None, "travelClass": None})
+            flights.append({"departure": {"iataCode": flight["source"].upper(), "at": format_departure_time(flight['date'].upper(), flight["departure_time"], 0)}, "arrival": {"iataCode": flight["destination"].upper(), "at": format_departure_time(flight['date'].upper(), flight["arrival_time"], days_offset)}, "flightNumber": flight["flight_number"], "carrierCode": flight["airline_code"].upper(), "duration": duration, "iteraryNumber": None, "travelClass": None})
     return {"offers": [{"passengers": None, "fares": [], "flights": flights, "geoCode": {}, "airportName": None, "cityCode": None, "upsellOffers": []}]}
 
 if __name__ == "__main__":
     text = """
-    3  DL9267 X 14APR 7 VCEAMS HK2          1140 1340   *1A/E*
-    4  DL 165 X 14APR 7 AMSMSP HK2          1520 1735   *1A/E*
-    5  DL1411 X 14APR 7 MSPYWG HK2       1  2157 2325   *1A/E*
-    6  AF8715 K 20APR 6 YWGMSP HK2  1150    1230 1402   *1A/E*
-    7  AF3574 K 20APR 6 MSPCDG HK2  1550 1  1630 0755+1 *1A/E*
-    8  AF1426 K 21APR 7 CDGVCE HK2  0900 2F 0940 1120   *1A/E*
+3  DL9267 X 14APR 7 VCEAMS HK2          1140 1340   *1A/E*
+  4  DL 165 X 14APR 7 AMSMSP HK2          1520 1735   *1A/E*
+  5  DL1411 X 14APR 7 MSPYWG HK2       1  2157 2325   *1A/E*
+  6  AF8715 K 20APR 6 YWGMSP HK2  1150    1230 1402   *1A/E*
+  7  AF3574 K 20APR 6 MSPCDG HK2  1550 1  1630 0755+1 *1A/E*
+  8  AF1426 K 21APR 7 CDGVCE HK2  0900 2F 0940 1120   *1A/E*
     """
 
     parsed_data = parse_offer_text(text)
